8e1e2092999be930d332d156633e1b05ed465c00
hariantara <hariantara.iputu@gmail.com>
2017-11-02 09:25:15 +0700

    get scrappingData done, but need put the url inside

--- tdd/controller/getScrappingData.js
+++ tdd/controller/getScrappingData.js
@@ -1,68 +1,73 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var getDataScrapping = (req, res) => {
-  request('http://www.tribunnews.com/tag/kecelakaan', function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-      var $ = cheerio.load(html);
-      var url
-      var arrUrl = []
-      $('.mr140 a').each(function(i, element){
-        url = $(this).attr('href')
-        dataTraversion = {
-          url: url
-        }
-        arrUrl.push(dataTraversion)
-        // console.log('===>', arrUrl);
-      })
+function mapURL (url, req, res) {
+  var count = 0
+  var arrData = []
+  url.forEach((dataText) => {
+    request(`${dataText.url}`, function (error, response, html) {
+      if(!error && response.statusCode == 200) {
+        var $ = cheerio.load(html)
+        var fixSpaceUsingRegex
 
-      // console.log('---->', arrUrl);
-      var result
-      var dataTampung = []
-      arrUrl.map(data => {
-        // console.log('---->',data.url);
-        request(data.url, (error, response, html) => {
-          if(!error && response.statusCode == 200) {
-            var $ = cheerio.load(html)
-            var fixSpaceUsingRegex
-            $('.content').each(function(i, element){
-              text = $(this).text()
-              fixSpaceUsingRegex = text.replace(/\s+/gm, ' ')
-              // console.log(fixSpaceUsingRegex);
-              datas = {
-                dataTexts: fixSpaceUsingRegex
-              }
-              dataTampung.push(datas)
-            })
-            // console.log('!!!!!!!!!!!!!!!',dataTampung);
-            result = dataTampung
-            console.log(result);
-            res.send(result)
+          $('.content').each(function(i, element){
+            text = $(this).text()
+            fixSpaceUsingRegex = text.replace(/\s+/gm, ' ')
+            // console.log(fixSpaceUsingRegex);
+            datas = {
+              dataTexts: fixSpaceUsingRegex
+            }
+            arrData.push(datas)
+            count +=1
+          })
+          if(count === url.length)
+          {
+            res.send(arrData)
           }
-        })
-      })
-    }
+      }
+    })
   })
 }
 
-var newsDetail = (req, res) => {
-  request('http://www.tribunnews.com/metropolitan/2017/10/29/kecelakaan-di-tol-dalam-kota-ini-ulah-pengemudi-pajero-yang-masih-anak-anak', function (error, response, html) {
-    if(!error && response.statusCode == 200) {
-      var $ = cheerio.load(html)
-      var fixSpaceUsingRegex
-      $('.content').each(function(i, element){
-        text = $(this).text()
-        fixSpaceUsingRegex = text.replace(/\s+/gm, ' ')
-        console.log(fixSpaceUsingRegex);
-        res.send(fixSpaceUsingRegex)
-      })
-    }
-  })
+var getDataScrapping = (req, res) => {
+
+    request('http://www.tribunnews.com/tag/kecelakaan', function (error, response, html) {
+     if (!error && response.statusCode == 200) {
+       var $ = cheerio.load(html);
+       var url
+       var arrUrl = []
+       $('.mr140 a').each(function(i, element){
+         url = $(this).attr('href')
+         dataTraversion = {
+           url: url
+         }
+         arrUrl.push(dataTraversion)
+       })
+       console.log('===>', arrUrl);
+       mapURL(arrUrl, req, res)
+     }
+   })
 }
+
+
+
+// var newsDetail = (req, res) => {
+//   request('http://www.tribunnews.com/metropolitan/2017/10/29/kecelakaan-di-tol-dalam-kota-ini-ulah-pengemudi-pajero-yang-masih-anak-anak', function (error, response, html) {
+//     if(!error && response.statusCode == 200) {
+//       var $ = cheerio.load(html)
+//       var fixSpaceUsingRegex
+//       $('.content').each(function(i, element){
+//         text = $(this).text()
+//         fixSpaceUsingRegex = text.replace(/\s+/gm, ' ')
+//         console.log(fixSpaceUsingRegex);
+//         res.send(fixSpaceUsingRegex)
+//       })
+//     }
+//   })
+// }
 
 
 
 module.exports = {
-  getDataScrapping,
-  newsDetail
+  getDataScrapping
 }
